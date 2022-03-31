c429640e9dcc45efdf567dd9d670215135a36588
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-03-31 14:45:28 +0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { env } from 'process';
 import { AppModule } from './app.module';
 import { TodoModule } from './todo/todo.module';
 
-const PORT = env.PORT;
+const PORT = process.env.PORT || env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
