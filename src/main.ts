0112645d984c33279cdea7b94c98432fac99a669
Ivan Avvakumov <66466689+avvakumovid@users.noreply.github.com>
2022-03-31 14:15:43 +0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import {NestFactory} from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
-import {TodoModule} from './todo/todo.module';
+import { TodoModule } from './todo/todo.module';
+
+const PORT = env.PORT;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
 }
 
 bootstrap();
